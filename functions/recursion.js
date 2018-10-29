ea8ca4f08d0c9eba37b36ef58f5a8e0226147feb
svyatlo <svyatlo@list.ru>
2018-10-29 21:56:50 +0300

    fix: fix bugs in function 'recursion'

--- functions/recursion.js
+++ functions/recursion.js
@@ -1,24 +1,27 @@
-const arrOfTree = [];
-const tempArr = [];
-const result = [];
-let count = 0;
-arrOfTree.push(tree);
 module.exports = function recursion(tree) {
-    let currentEl = arrOfTree.shift();
-    tempArr.push(currentEl.value);
-    if (currentEl.left){
-        arrOfTree.push(currentEl.left);
-    }  
-    if (currentEl.right) {
-        arrOfTree.push(currentEl.right);
-    }
-    if (tempArr.length === Math.pow(2, count)) {
-        result.push(tempArr);
-        tempArr = [];
-        count += 1;
-    }
-    for (let i = 0; i < arrOfTree.length; i += 1) {
-        recursion(arrOfTree[i]);
-    }
-    return result;
-}
+    let count = 0;
+    let tempArr = [];
+    const arrOfTree = [];
+    const tempResult = [];
+    arrOfTree.push(tree);
+    const result = function repeat() {
+        const currentEl = arrOfTree.shift();
+        tempArr.push(currentEl.value);
+        if (currentEl.left) {
+            arrOfTree.push(currentEl.left);
+        }
+        if (currentEl.right) {
+            arrOfTree.push(currentEl.right);
+        }
+        if (tempArr.length === Math.pow(2, count)) {
+            tempResult.push(tempArr);
+            tempArr = [];
+            count += 1;
+        }
+        for (let i = 0; i < arrOfTree.length; i += 1) {
+            repeat(arrOfTree[i]);
+        }
+        return tempResult;
+    };
+    return result();
+};
